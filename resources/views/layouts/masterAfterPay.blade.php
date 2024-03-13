2a69638eabb56c6074d1981353dd4f824200dd04
nopal091 <priatna091@gmail.com>
2024-03-13 12:08:41 +0700

    layout navbar dan sidebar Admin Univ After Payment

--- resources/views/layouts/masterAfterPay.blade.php
+++ resources/views/layouts/masterAfterPay.blade.php
@@ -8,8 +8,6 @@
     {{-- fontawesome --}}
     <script src="https://kit.fontawesome.com/edd6108211.js" crossorigin="anonymous"></script>
 {{-- bs --}}
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('assets/css/layout.css') }}">
@@ -17,8 +15,6 @@
     <link rel="stylesheet" href="https://cdn.datatables.net/2.0.1/css/dataTables.bootstrap5.css">
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
-<script src="https://code.jquery.com/jquery-3.7.1.js"></script>
-
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed">
@@ -34,7 +30,7 @@
         </div>
 
         <aside class="main-sidebar">
-            @include('template.sidebar') <!-- bueeek master baru piqri bangke -->
+            @include('template.sidebarAfterPay') <!-- bueeek master baru piqri bangke -->
         </aside>
 
         <aside class="control-sidebar control-sidebar-dark">
@@ -50,15 +46,17 @@
     <script src="https://cdn.datatables.net/2.0.1/js/dataTables.js"></script>
     <script src="https://cdn.datatables.net/2.0.1/js/dataTables.bootstrap5.js"></script>
     <script>
+
         $(document).ready( function () {
             var table = $('#example').DataTable( {
                 pageLength : 5,
                 lengthMenu: [[5, 7, 10, 50, -1], [5, 7, 10, 50, 'semua']],
                 language: {
-                    searchPlaceholder: 'cari nama mahasiswa'
+                    searchPlaceholder: 'search here..'
                 }
              } )
         } );
     </script>
 </body>
+
 </html>
