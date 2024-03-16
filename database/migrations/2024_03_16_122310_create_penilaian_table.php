703368a0f8c86f80e48995551aed020b25767206
naufalpratam4 <naufal.d.pratama@gmail.com>
2024-03-16 20:05:58 +0700

    add KategoriPenilaian, Penilaian, SubKategoriPenilaian seeder and add migration kategori_penilaian, sub_kategori, penilaian

---
+++ database/migrations/2024_03_16_122310_create_penilaian_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('penilaian', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('nama_lengkap');
+            $table->unsignedBigInteger('sub_id');
+            $table->integer('nilai');
+            $table->text('kritik_saran');
+            $table->timestamps();
+            $table->foreign('nama_lengkap')->references('id')->on('users');
+            $table->foreign('sub_id')->references('id')->on('sub_kategori_penilaian');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('penilaian');
+    }
+};
