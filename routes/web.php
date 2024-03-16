796d0adb085c20765eb2df5c1dc694dcd45b72b5
alyarusydam@gmail.com <alyarusydam@gmail.com>
2024-03-16 09:45:10 +0700

    create: [user] homepage, barcode login

--- routes/web.php
+++ routes/web.php
@@ -157,7 +157,6 @@ Route::get('/user', function () {
         'divisi' =>  "MJ/UIUX/POLINES/AGST 2023/06"
     ]);
 });
-
 Route::get('/user/barcode', function () {
     return view('user.barcode', [
         'title' => "Barcode Pemagang",
