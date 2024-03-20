6cc47fcbb158cb515becbf8149723a86c8cef100
aldi12356 <aldiapoy59@gmail.com>
2024-03-20 20:26:24 +0700

    User Admin Sistem/Lokasi Dashboard,Dashboard - Edit Profile,Subscription

---
+++ resources/views/template/sidebarSistem.blade.php
@@ -0,0 +1,20 @@
+<link rel="stylesheet" href="assets/Css/sidebar.css">
+<div class="sidebar">
+    <img src="assets/images/logo.png" alt="Logo" class="logo">
+    <ul class="nav flex-column">
+        <li class="nav-item">
+
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/AdminSistem-Dashboard">Dashboard</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/AdminSistem-Subcription">Langganan</a>
+        </li>
+        <li class="nav-item">
+            <div class="log-out align-items-center gap-3 d-flex flex-row w-100 justify-content-center logout">
+                <b>Log Out</b> <i class="fa-solid fa-right-from-bracket"></i>
+            </div>
+        </li>
+    </ul>
+</div>
