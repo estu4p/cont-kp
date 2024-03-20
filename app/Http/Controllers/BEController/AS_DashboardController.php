2552bd001aaf1139ff99a5144ea1d9ecb3d89cdf
unknown <bella nesinta>
2024-03-20 12:06:52 +0700

    update

---
+++ app/Http/Controllers/BEController/AS_DashboardController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\BEController;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\MitraMahasiswa;
+
+class AS_DashboardController extends Controller
+{
+    public function filterDashboard()
+    {
+        // Menghitung total subcription
+        $totalSubscription = MitraMahasiswa::count();
+        
+        // Menghitung total aktif dan tidak aktif
+        $totalAktif = MitraMahasiswa::where('jumlah_subcription', 'aktif')->count();
+        $totalTidakAktif = MitraMahasiswa::where('jumlah_subcription', 'tidak aktif')->count();
+
+        return response()->json([
+            'total_subcription' => $totalSubcription,
+            'total_aktif' => $totalAktif,
+            'total_tidak_aktif' => $totalTidakAktif,
+        ]);
+    }
+}
