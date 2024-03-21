cbd7f5d9711a8c2b18dc3c390f6f635516ed85bd
naufalpratam4 <naufal.d.pratama@gmail.com>
2024-03-21 09:03:47 +0700

    update migration and seeder

--- database/migrations/2024_03_16_000000_create_users_table.php
+++ database/migrations/2024_03_16_000000_create_users_table.php
@@ -27,6 +27,7 @@ return new class extends Migration
             $table->string('about')->nullable();
             $table->string('os')->nullable();
             $table->enum('status_akun', ['aktif', 'alumni'])->nullable();
+            $table->enum('status_absensi', ['Scan QR Code', 'Button']);
             $table->string('browser')->nullable();
             $table->date('tgl_masuk')->nullable();
             $table->date('tgl_keluar')->nullable();
