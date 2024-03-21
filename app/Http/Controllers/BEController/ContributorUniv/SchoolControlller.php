02e45983afb98806515ef52551dd52e0132522c9
0512-0221 <laylatululla@gmail.com>
2024-03-21 11:24:37 +0700

    Dasboar uinv

--- app/Http/Controllers/BEController/ContributorUniv/SchoolControlller.php
+++ app/Http/Controllers/BEController/ContributorUniv/SchoolControlller.php
@@ -16,12 +16,15 @@ class ContributorUnivController extends Controller
     { // menampilkan seluruh data yang diperlukan
         $jumlah_Mahasiswa = User::where("role_id", 3)->get();
         $kehasiran_masuk= Presensi::where("status_kehadiran", 'hadir')->get();
-        $kehadiran_izin=Presensi::where("status_kehadiran", 'izin');
+        $kehadiran_izin=Presensi::where("status_kehadiran", 'izin')->get();
 
         // Mengambil mahasiswa dari koleksi data
 
         return response()->json([
             "message" => "Success get data",
+            "jumlah Mahasiswa"=> $jumlah_Mahasiswa,
+            "jumlah Masuk"=>$kehasiran_masuk,
+            "jumlah Izin"=>$kehadiran_izin, 
 
         ]);
     }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LandingPageController;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\BEController\ContributorUnivController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\ResetPasswordController;
 use App\Http\Controllers\BEController\AdminUnivAfterPaymentController;
