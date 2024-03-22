e1cc0f453962a4f2dd2e864c27bcf884c3861907
0512-0221 <laylatululla@gmail.com>
2024-03-22 11:46:47 +0700

    cu

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,17 @@
 <?php
 
 use Illuminate\Http\Request;
+use BEController\SchoolController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ApiAuthController;
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\LandingPageController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\BEController\ContributorUnivController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\ResetPasswordController;
 use App\Http\Controllers\BEController\AdminUnivAfterPaymentController;
-use App\Http\Controllers\BEController\HomeMitraController;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
