3d63d602f0953b25a14677c73a514da589e8ada4
0512-0221 <laylatululla@gmail.com>
2024-03-23 11:52:52 +0700

    Done Contributor Univ

--- app/Http/Controllers/BEController/SchoolController.php
+++ app/Http/Controllers/BEController/SchoolController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\BEController;
 
 use App\Models\User;
 use App\Models\Presensi;
+use App\Models\Mahasiswa;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -11,7 +12,7 @@ class SchoolController extends Controller
 {
     public function index()
     { // menampilkan seluruh data yang diperlukan
-        $jumlah_Mahasiswa = User::where("role_id", 3)->get();
+        $jumlah_Mahasiswa = User::where("role_id", 3)->count();
         $kehasiran_masuk= Presensi::where("status_kehadiran", 'hadir')->count();
         $kehadiran_izin= Presensi::where("status_kehadiran", 'izin')->count();
 
@@ -22,4 +23,16 @@ class SchoolController extends Controller
             "jumlah Izin" =>$kehadiran_izin
         ]);
     }
+    public function jumlahMahasiswa()
+    {
+        $JM = User::where("role_id",3)
+                ->select( "nama_lengkap", "nomor_induk","divisi_id","status_akun")
+                ->get();
+
+        return response()->json([
+            "jumlah Mahasiswa" => "view data Mahasiswa ",
+            "data" => $JM
+        ]);
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Http\Request;
-use BEController\SchoolController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ApiAuthController;
@@ -9,13 +8,14 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\LandingPageController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Controllers\BEController\SchoolController;
+use App\Http\Controllers\BEController\SchoolControlller;
+use App\Http\Controllers\BEController\ContributorForMitra;
 use App\Http\Controllers\BEController\DataMitraController;
 use App\Http\Controllers\BEController\HomeMitraController;
 use App\Http\Controllers\BEController\DashboardAdminController;
 use App\Http\Controllers\BEController\ContributorUnivController;
-use App\Http\Controllers\BEController\SchoolControlller;
 use App\Http\Controllers\BEController\AdminUnivAfterPaymentController;
-use App\Http\Controllers\BEController\ContributorForMitra;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,10 +34,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::post('/loginn', [ApiAuthController::class, 'login']);
-
+//landing page
 Route::post('/daftar', [LandingPageController::class, 'lpdaftar']);
 Route::post('/loginpage', [LandingPageController::class, 'login'])->name('login');
 Route::post('/ChekoutPaket', [LandingPageController::class, 'ChekoutPaket'])->name('paket');
+
 Route::post('/user/login', [LoginController::class, 'validateLogin'])->name('user.login');
 Route::post('/user/register', [RegisterController::class, 'register'])->name('register');
 Route::post('/user/register/showRegisterForm', [RegisterController::class, 'showRegisterForm']);
@@ -83,7 +84,8 @@ Route::get('admin/daftar-mitra/detail-hadir/{id}', [AdminUnivAfterPaymentControl
 Route::get('admin/daftar-mitra/laporan-data-presensi', [AdminUnivAfterPaymentController::class, 'laporanDataPresensi']);
 
 //Contributor for univ
-Route::get('/dashboard-univ', [\App\Http\Controllers\BEController\SchoolController::class, 'index']);
+Route::get('/dashboard-univ', [SchoolController::class, 'index']);
+Route::get('/jumlahmahasiswa', [SchoolController::class, 'jumlahMahasiswa']);
 
 //Contributor for Mitra
 Route::get('daftar-divisi', [ContributorForMitra::class,'daftarDivisi']);
