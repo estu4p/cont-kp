f8bec0ca93886efe513961b027672502434d6e16
nopal091 <priatna091@gmail.com>
2024-03-30 13:55:41 +0700

    memperbaiki revisi landing page yang bagian Checkout - Bronze

--- resources/views/checkout/bronze.blade.php
+++ resources/views/checkout/bronze.blade.php
@@ -40,35 +40,47 @@
                     <div style="background-color: #E9E9E9; padding: 14px 15px 17px 14px;" class="label-img"><img
                             src="{{ asset('assets/images/icon/bayar.png') }}" style="width: 25px" class="icon-labelinput">
                     </div>
-                    {{-- <select style="border: 2px solid #E9E9E9; padding: 16px; border-radius: 0px 8px 8px 0px; width: 100%;"
-                        id="bayar">
-                        <option value="">Pilih Metode Pembayaran</option> --}}
-                    <select id="selectKota" type="email" name="kota"
-                        style="border: 2px solid #E9E9E9; padding: 16px; border-radius: 0px 8px 8px 0px; width: 100%;">
-                        <option value="" selected disabled hidden>Pilih Metode Pembayaran</option>
-                        <option value="mandiri">Mandiri Virtual Account</option>
-                        <option value="bca">BCA Virtual Account</option>
-                        <option value="bri">BRI Virtual Account</option>
-                        <option value="BNI">BNI Virtual Account</option>
-                        <option value="shopeepay">Shopeepay</option>
-                        <option value="gopay">Gopay</option>
-                    </select>
+                    <button id="pilihMetode" class="btn" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample" style="border: 2px solid #E9E9E9; padding: 15px; border-radius: 0px 8px 8px 0px; width: 100%;">
+                        Pilih Metode Pembayaran<i class="fa-solid fa-caret-down" style="padding-left: 40%"></i>
+                      </button>
                 </div>
 
-                <label for="kota" class="mt-4">kota<span class="text-danger">*</span></label>
+                <div class="collapse" id="collapseExample">
+                    <div class="dalam card-body" style="display: flex">
+                      <div>
+                        <div class="list-pembayaran"><input type="radio" class="radio" name="metode" value="Mandiri Virtual Account"> Mandiri Virtual Account <img src="/assets/images/icon/mandiri.png" > </div>
+                        <div class="list-pembayaran"><input type="radio" class="radio" name="metode" value="BNI Virtual Account"> BNI Virtual Account <img src="/assets/images/icon/bni.png"></div>
+                        <div class="list-pembayaran" ><input type="radio" class="radio" name="metode" value="Shopee Pay"> Shopee Pay <img src="/assets/images/icon/shopeePay.png" ></div>
+                      </div>
+                      <div>
+                        <div class="list-pembayaran"><input type="radio" class="radio" name="metode" value="BCA Virtual Account"> BCA Virtual Account <img src="/assets/images/icon/bca.png" ></div>
+                        <div class="list-pembayaran"><input type="radio" class="radio" name="metode" value="BRI Virtual Account"> BRI Virtual Account <img src="/assets/images/icon/bri.png" ></div>
+                        <div class="list-pembayaran"><input type="radio" class="radio" name="metode" value="GoPay"> GoPay <img src="/assets/images/icon/gopay.png" ></div>
+                      </div>
+                    </div>
+                  </div>
+
+
+                <label for="kota" class="mt-4">lokasi<span class="text-danger">*</span></label>
                 <div class="d-flex">
                     <div style="background-color: #E9E9E9; padding: 14px 15px 17px 14px;" class="label-img"><img
                             src="{{ asset('assets/images/icon/kota.png') }}" style="width: 25px" class="icon-labelinput">
                     </div>
-                    <select id="selectKota" type="email" name="kota"
-                        style="border: 2px solid #E9E9E9; padding: 16px; border-radius: 0px 8px 8px 0px; width: 100%;">
-                        <option value="" selected disabled hidden>Pilih Kota</option>
-                        <option value="yogyakarta" type="radio">Yogyakarta</option>
-                        <option value="jawa tengah">Jawa Tengah</option>
-                        <option value="jawa barat">Jawa Barat</option>
-                        <option value="jawa timur">Jawa Timur</option>
-                    </select>
+
+                    <button id="pilihKotaButton" class="btn" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample2" aria-expanded="false" aria-controls="collapseExample" style="border: 2px solid #E9E9E9; padding: 15px; border-radius: 0px 8px 8px 0px; width: 100%;">
+                        Pilih Kota Anda
+                    </button>
                 </div>
+                <div class="collapse" id="collapseExample2">
+                    <div class="dalam2 card-body">
+                        Yogyakarta <input type="radio" name="option" value="Yogyakarta" style="float: Right;"><br>
+                        Jawa Tengah <input type="radio" name="option" value="Jawa Tengah" style="float: Right;"><br>
+                        Jawa Barat <input type="radio" name="option" value="Jawa Barat" style="float: Right;"><br>
+                        Jawa Timur <input type="radio" name="option" value="Jawa Timur" style="float: Right;"><br>
+                    </div>
+                </div>
+
+
 
                 <div class="button-container">
                     <button class="border-0 mt-5 fs-5 fw-semibold"
@@ -78,20 +90,61 @@
                 </div>
             </form>
         </div>
+        <br>
         <div style="text-align: center">
-        <p>Dengan melakukan checkout, maka Anda setuju dengan<br><p style="color: red">Ketentuan Penggunaan kami</p>dan mengonfirmasi bahwa<br>Anda telah membaca<p style="color:red">Kebijakan Privasi</p>kami.</p>
-    </div>
+            <p>Dengan melakukan checkout, maka Anda setuju dengan<br>
+                <span style="color: red;">Ketentuan Penggunaan kami</span> dan mengonfirmasi bahwa<br>
+                Anda telah membaca<span style="color: red;"> Kebijakan Privasi</span> kami.
+            </p>
+        </div>
     </div>
 
-    <footer style="background-color: #A61C1CE5; margin-bottom: -48px; position: absolute; bottom: 0; width: 100%;">
-        <p class="text-center text-uppercase fs-6 my-auto py-2 text-white">
+    <footer style="background-color: #A61C1CE5; width: 100%;">
+        <div class="text-center text-uppercase fs-6 my-auto py-2 text-white">
             <span class="fs-3 fw-bold" style="vertical-align: middle;">&copy;</span> 2023 pt.seven inc
-        </p>
+        </div>
     </footer>
+
 
     <script>
         document.getElementById("yakinBtn").addEventListener("click", function() {
             window.location.href = "/after-checkout";
         });
     </script>
+
+
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+      const radios = document.querySelectorAll('.radio');
+      const button = document.getElementById('pilihMetode');
+      const collapseExample = document.getElementById('collapseExample');
+
+      radios.forEach(radio => {
+        radio.addEventListener('change', function() {
+          if (this.checked) {
+            button.textContent = ' ' + this.value;
+            collapseExample.classList.remove('show'); // Menghilangkan kelas 'show' dari elemen collapse
+          }
+        });
+      });
+    });
+  </script>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const pilihKotaButton = document.getElementById('pilihKotaButton');
+        const radios = document.getElementsByName('option');
+        const collapseExample = document.getElementById('collapseExample2');
+
+        radios.forEach(radio => {
+            radio.addEventListener('change', function() {
+                if (this.checked) {
+                    pilihKotaButton.innerText = this.value;
+                    collapseExample.classList.remove('show');
+                }
+            });
+        });
+    });
+</script>
+
 @endsection
