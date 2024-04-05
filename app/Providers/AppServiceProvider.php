1118b5fed6b780a8235e2348f0b8f32de783b420
estu <rezapramudya112@gmail.com>
2024-04-05 11:07:22 +0700

    memberi variabel  ke navbar-super

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // membari variabel $superAdmin ke navbar-super
+        View::composer('template.navbar-super', function ($view) {
+            $superAdmin = User::where('role_id', 1)->first();
+            $view->with('superAdmin', $superAdmin);
+        });
     }
 }
