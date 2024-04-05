85d20cca48c371a85c92f79ea645d30097c6d08a
0512-0221 <laylatululla@gmail.com>
2024-04-05 12:34:33 +0700

    ya

--- app/Models/project.php
+++ app/Models/project.php
@@ -8,11 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
-    protected $fillable = ['nama_project', 'deskripsi', 'nama_tim', 'tgl_mulai', 'tgl_selesai'];
     protected $table = 'project';
-
-    public function project()
-    {
-        return $this->hasMany(User::class, 'project_id')->where('role_id', 3);
-    }
+    protected $fillable = [
+        'nama_project',
+        'deskripsi_project',
+        'nama_tim',
+        'tgl_mulai',
+        'tgl_selesai',
+    ];
 }
