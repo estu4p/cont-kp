1dc27c7fc6950c5905726b74f2784851117f5c9e
IqraAulia <iqraauliasaputra@gmail.com>
2024-04-18 09:41:22 +0700

    responsive landing page

--- resources/views/landing-page/index.blade.php
+++ resources/views/landing-page/index.blade.php
@@ -158,7 +158,7 @@
     <h1 class="text-capitalize  text-center mt-4">pilih paket sesuai kebutuhan anda</h1>
     <div class="container text-center py-5">
         <div class="row align-items-center justify-content-around">
-            <div class="col-xl-3 col-lg-5 col-md-6 col-sm-10 border border-danger">
+            <div class="col-xl-3 col-lg-5 col-md-6 col-sm-10 ">
                 <div class="card-paket card p-2 pb-4" style=" border-radius: 16px;">
                     <div class="card-body mt-2">
                         <h5 class="fw-bolder text-uppercase" style="font-size: 24px;">bronze
@@ -166,7 +166,7 @@
                         <h4 class="fw-bolder text-uppercase   " style=" white-space: nowrap;color: #A61C1CE5; opacity: 90%; margin-top: 36px;">Rp 1.000.000
                         </h4>
                         <p class="card-text px-3" style="font-size: 18px; opacity: 70%;">/tahun</p>
-                        <div class="mt-4 mb-5 border border-success" style=" text-align: start;">
+                        <div class="mt-4 mb-5  height" style=" text-align: start;">
                             <p><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-check2-square me-2" style="color: #A61C1CE5;" viewBox="0 0 16 16">
                                     <path d="M3 14.5A1.5 1.5 0 0 1 1.5 13V3A1.5 1.5 0 0 1 3 1.5h8a.5.5 0 0 1 0 1H3a.5.5 0 0 0-.5.5v10a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V8a.5.5 0 0 1 1 0v5a1.5 1.5 0 0 1-1.5 1.5z" />
                                     <path d="m8.354 10.354 7-7a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0" />
@@ -194,7 +194,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-xl-3 col-lg-5 col-md-6 col-sm-10 border border-danger">
+            <div class="col-xl-3 col-lg-5 col-md-6 col-sm-10 ">
                 <div class="card-paket card p-2 pb-4" style=" border-radius: 16px;">
                     <div class="card-body mt-2">
                         <h5 class="fw-bolder text-uppercase" style="font-size: 24px;">silver
@@ -202,7 +202,7 @@
                         <h4 class="fw-bolder text-uppercase   " style="font-size:170% !important;white-space:nowarp;color: #0038FFE5; opacity: 90%; margin-top: 36px;">Rp 4.000.000
                         </h4>
                         <p class="card-text px-3" style="font-size: 18px; opacity: 70%;">/tahun</p>
-                        <div class="mt-4 mb-5 border border-success" style=" text-align: start;">
+                        <div class="mt-4 mb-5  height " style=" text-align: start;">
                             <p><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-check2-square me-2" style="color: #0038FFE5;" viewBox="0 0 16 16">
                                     <path d="M3 14.5A1.5 1.5 0 0 1 1.5 13V3A1.5 1.5 0 0 1 3 1.5h8a.5.5 0 0 1 0 1H3a.5.5 0 0 0-.5.5v10a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V8a.5.5 0 0 1 1 0v5a1.5 1.5 0 0 1-1.5 1.5z" />
                                     <path d="m8.354 10.354 7-7a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0" />
@@ -230,7 +230,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-xl-3 col-lg-5 col-md-6 col-sm-10 border border-danger">
+            <div class="col-xl-3 col-lg-5 col-md-6 col-sm-10 ">
                 <div class="card-paket card p-2 pb-4" style=" border-radius: 16px;">
                     <div class="card-body mt-2">
                         <h5 class="fw-bolder text-uppercase" style="font-size: 24px;">gold
@@ -238,7 +238,7 @@
                         <h4 class="fw-bolder text-uppercase   " style="font-size:170% !important;white-space:nowarp;color: #009745E5; opacity: 90%; margin-top: 36px;">Rp 7.000.000
                         </h4>
                         <p class="card-text px-3" style="font-size: 18px; opacity: 70%;">/tahun</p>
-                        <div class="mt-4 mb-5 border border-success" style=" text-align: start;">
+                        <div class="mt-4 mb-5  height" style=" text-align: start;">
                             <p><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-check2-square me-2" style="color: #009745E5;" viewBox="0 0 16 16">
                                     <path d="M3 14.5A1.5 1.5 0 0 1 1.5 13V3A1.5 1.5 0 0 1 3 1.5h8a.5.5 0 0 1 0 1H3a.5.5 0 0 0-.5.5v10a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V8a.5.5 0 0 1 1 0v5a1.5 1.5 0 0 1-1.5 1.5z" />
                                     <path d="m8.354 10.354 7-7a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0" />
@@ -266,7 +266,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-xl-3 col-lg-5 col-md-6 col-sm-10 border border-danger">
+            <div class="col-xl-3 col-lg-5 col-md-6 col-sm-10 ">
                 <div class="card-paket card p-2 pb-4" style=" border-radius: 16px;">
                     <div class="card-body mt-2">
                         <h5 class="fw-bolder text-uppercase" style="font-size: 24px;">platinum
@@ -274,7 +274,7 @@
                         <h4 class="fw-bolder text-uppercase   " style="font-size:170% !important;white-space:nowarp;color: #36007BE5; opacity: 90%; margin-top: 36px;">Rp 10.000.000
                         </h4>
                         <p class="card-text px-3" style="font-size: 18px; opacity: 70%;">/tahun</p>
-                        <div class="mt-4 mb-5 border border-success" style=" text-align: start;">
+                        <div class="mt-4 mb-5  height" style=" text-align: start;">
                             <p><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-check2-square me-2" style="color: #36007BE5;" viewBox="0 0 16 16">
                                     <path d="M3 14.5A1.5 1.5 0 0 1 1.5 13V3A1.5 1.5 0 0 1 3 1.5h8a.5.5 0 0 1 0 1H3a.5.5 0 0 0-.5.5v10a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V8a.5.5 0 0 1 1 0v5a1.5 1.5 0 0 1-1.5 1.5z" />
                                     <path d="m8.354 10.354 7-7a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0" />
@@ -309,66 +309,70 @@
 <footer id="footer" class="bg-white">
     <div class="text-white py-4" style="background-color: #A61C1CE5;">
         <div class="container container-footer">
-            <div class="row align-items-center">
-                <div class="col-4 border border-ligt untukmobile">
+            <div class="row align-items-center min767">
+                <div class="col-4  untukmobile">
                     <div class="d-flex untukmobilelogo justify-content-center">
-                        <div class=" border border-light "><img src="assets/images/logo.png" width="70px" />
-                        <h4 class="text-uppercase my-auto ms-3 ms-sm-0 my-sm-0 fw-bold border border-light">seven.inc</h4></div>
-                        
+                        <div class="  "><img src="assets/images/logo.png" width="70px" />
+                            <h4 class="text-uppercase my-auto ms-3 ms-sm-0 my-sm-0 fw-bold ">seven.inc</h4>
+                        </div>
+
                     </div>
                     <p class="mt-4">+6282934397492</p>
                     <p class="flex-wrap  untukmobiletext" style="margin-top: -12px;">Jalan Raya Janti, Gang Arjuna Nomor 59,
                         Karangjambe, Banguntapan, Bantul
                         Regency, Special Region of Yogyakarta 55198</p>
-                    <div class="sosmed567 border border-light">
-                        <div class="d-flex gap-2  border border-light" style="width: max-content;">
-                        <a href="" style="color: #A61C1CE5; background-color: white; width: 28px; height: 28px; border-radius: 100%; text-align: center;">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
-                                <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z" />
-                            </svg>
-                        </a>
-                        <a href="" style="color: #A61C1CE5; background-color: white; width: 28px; height: 28px; border-radius: 100%; text-align: center;">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter-x" viewBox="0 0 16 16">
-                                <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z" />
-                            </svg>
-                        </a>
-                        <a href="" style="color: #A61C1CE5; background-color: white; width: 28px; height: 28px; border-radius: 100%; text-align: center;">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16">
-                                <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334" />
-                            </svg>
-                        </a>
-                        <a href="" style="background-color: #A61C1CE5; border-radius: 100%; text-align: center;">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-facebook" style="color: white;" viewBox="0 0 16 16">
-                                <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
-                            </svg>
-                        </a>
-                        <a href="" style="background-color: #A61C1CE5; text-align: center;">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-linkedin" style="color: white; border-radius: 100%;" viewBox="0 0 16 16">
-                                <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
-                            </svg>
-                        </a>
+                    <div class="sosmed767 ">
+                        <div class="d-flex gap-2  " style="width: max-content;">
+                            <a href="" style="color: #A61C1CE5; background-color: white; width: 28px; height: 28px; border-radius: 100%; text-align: center;">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+                                    <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z" />
+                                </svg>
+                            </a>
+                            <a href="" style="color: #A61C1CE5; background-color: white; width: 28px; height: 28px; border-radius: 100%; text-align: center;">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter-x" viewBox="0 0 16 16">
+                                    <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z" />
+                                </svg>
+                            </a>
+                            <a href="" style="color: #A61C1CE5; background-color: white; width: 28px; height: 28px; border-radius: 100%; text-align: center;">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16">
+                                    <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334" />
+                                </svg>
+                            </a>
+                            <a href="" style="background-color: #A61C1CE5; border-radius: 100%; text-align: center;">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-facebook" style="color: white;" viewBox="0 0 16 16">
+                                    <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
+                                </svg>
+                            </a>
+                            <a href="" style="background-color: #A61C1CE5; text-align: center;">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-linkedin" style="color: white; border-radius: 100%;" viewBox="0 0 16 16">
+                                    <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
+                                </svg>
+                            </a>
+                        </div>
                     </div>
+                </div>
+                <div class="tengahr ">
+                    <!-- <div class="col-2 "></div> -->
+                    <div class="col-2 text-capitalize ">
+                        <h3 class="mb-4">fitur</h3>
+                        <p>presensi siswa</p>
+                        <p>penilaian siswa</p>
+                        <p>pantau siswa</p>
+                        <p>pengaturan team</p>
+                    </div>
+                    <div class="col-2 text-capitalize">
+                        <h3 class="mb-4">harga</h3>
+                        <p>bronze</p>
+                        <p>silver</p>
+                        <p>gold</p>
+                        <p>platinum</p>
+                    </div>
+                    <div class="col-2 text-capitalize">
+                        <h3 class="mb-4">tools bisnis</h3>
+                        <p>presensi siswa</p>
                     </div>
                 </div>
-                <div class="col-2 border border-light"></div>
-                <div class="col-2 text-capitalize border border-light">
-                    <h3 class="mb-4">fitur</h3>
-                    <p>presensi siswa</p>
-                    <p>penilaian siswa</p>
-                    <p>pantau siswa</p>
-                    <p>pengaturan team</p>
-                </div>
-                <div class="col-2 text-capitalize">
-                    <h3 class="mb-4">harga</h3>
-                    <p>bronze</p>
-                    <p>silver</p>
-                    <p>gold</p>
-                    <p>platinum</p>
-                </div>
-                <div class="col-2 text-capitalize">
-                    <h3 class="mb-4">tools bisnis</h3>
-                    <p>presensi siswa</p>
-                </div>
+
             </div>
         </div>
     </div>
