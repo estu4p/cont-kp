c5cc93368111eeefe7edea5bea173d7410c4d0cc
IqraAulia <iqraauliasaputra@gmail.com>
2024-04-30 09:20:04 +0700

    menambahkan lingkarang untuk tempat count notifnya all navbar

--- resources/views/template/navbarSistem.blade.php
+++ resources/views/template/navbarSistem.blade.php
@@ -7,7 +7,10 @@
       <div class="kosongan"></div>
       <div class="kanan  d-flex flex-row justify-content-between gap-5">
         <div class="ml-5  mr-2">
-          <i class="bi bi-bell" style="font-size: 30px;"></i>
+          <i class="bi bi-bell" style="font-size: 30px; position: relative;">
+            <!-- Bulatan notifikasi -->
+            <span class="badge">3</span>
+          </i>
         </div>
         <a href="/AdminSistem-Editprofile" style="text-decoration: none; color: #000;">
           <div class="d-flex justify-content-evenly flex-row ">
@@ -22,4 +25,20 @@
         </a>
       </div>
     </nav>
+    <script>
+      // Ambil elemen navbar
+      const navbar = document.querySelector('.navbar');
+
+      // Tambahkan event listener untuk mendeteksi scroll
+      window.addEventListener('scroll', function() {
+        // Periksa posisi scroll
+        if (window.scrollY > 0) {
+          // Jika posisi scroll lebih dari 0, tambahkan class 'navbar-scrolled'
+          navbar.classList.add('navbar-scrolled');
+        } else {
+          // Jika tidak, hapus class 'navbar-scrolled'
+          navbar.classList.remove('navbar-scrolled');
+        }
+      });
+    </script>
   </body>
