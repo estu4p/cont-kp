800f43846b067ddb76c2ebedcc751cfd52b2de96
adriansyah1 <adriansyah@students.amikom.ac.id>
2024-05-08 13:28:14 +0700

    update tampilan admin sistem dashboard

--- resources/views/SistemLokasi/AdminSistem-Dashboard.blade.php
+++ resources/views/SistemLokasi/AdminSistem-Dashboard.blade.php
@@ -4,40 +4,28 @@
 <link rel="stylesheet" href="{{ asset('assets/css/AdminSistem-Dashboard.css') }}">
 
 <body>
-  <div class="dashboard">
-    <div class="kartu1 d-flex flex-column">
-      <h3 class="kartu-judul">Jumlah Subcription</h3>
-       <td class=>{{ $totalSubscription }}</td>
-      
-  
-
-      <!-- <i class="bi bi-check2-circle text-dark icon1 col-12 d-flex justify-content-end py-2 px-4 "></i> -->
-
-    </div>
-
-
-
-    <div class=" kartu2 d-flex flex-column">
-      <h2 class="kartu-judul">Aktif</h2>
-      <td class=>{{ $totalAktif }}</td>
-        <!-- <img src="assets/images/vectorr.png"alt="logoo" class="vectorr">
+    <div class="dashboard flex-md-column">
+        <div class=" gap-5  p-5 d-flex  flex-lg-row flex-md-column flex-sm-column  flex-column">
+            <div class="kartu1 m-lg-0 m-md-auto m-sm-auto my-md-3">
+                <h2 class="kartu-judul">Jumlah Subcription</h2>
+                <td>{{ $totalSubscription }}</td>
+                <!-- <i class="bi bi-check2-circle text-dark icon1 col-12 d-flex justify-content-end py-2 px-4 "></i> -->
+            </div>
+            <div class="kartu2 m-lg-0 m-md-auto m-sm-auto my-md-3">
+                <h2 class="kartu-judul">Aktif</h2>
+                <td>{{ $totalAktif }}</td>
+                <!-- <img src="assets/images/vectorr.png"alt="logoo" class="vectorr">
         <img src="assets/images/Vector3.png" alt="panah" class="panah"> -->
-
-
-      <!-- <i class="bi bi-box-arrow-in-right text-dark icon2 col-12 d-flex justify-content-end py-2 px-4"></i> -->
-    </div>
-
-    <div class="kartu3 dm-flex flex-colum ">
-      <h2 class="kartu-judul">Tidak aktif</h2>
-        <td>{{ $totalTidakAktif }}</td>
-
-      
-      <!-- <i class="bi bi-app text-dark icon3 col-12 d-flex justify-content-end py-2 px-4">
+                <!-- <i class="bi bi-box-arrow-in-right text-dark icon2 col-12 d-flex justify-content-end py-2 px-4"></i> -->
+            </div>
+            <div class="kartu3 m-lg-0 m-md-auto m-sm-auto my-md-3">
+                <h2 class="kartu-judul">Tidak aktif</h2>
+                <td>{{ $totalTidakAktif }}</td>
+                <!-- <i class="bi bi-app text-dark icon3 col-12 d-flex justify-content-end py-2 px-4">
         <i class="bi bi-x text-dark icon4 p-0"></i>
       </i> -->
-
-
+            </div>
+        </div>
     </div>
-  </div>
 </body>
 @endsection
--- resources/views/user/AdminUnivAfterPayment/RiwayatPembelian.blade.php
+++ resources/views/user/AdminUnivAfterPayment/RiwayatPembelian.blade.php
@@ -1,45 +1,43 @@
 @extends('layouts.masterAfterPay')
 
 @section('content')
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://kit.fontawesome.com/edd6108211.js" crossorigin="anonymous"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<script src="https://kit.fontawesome.com/edd6108211.js" crossorigin="anonymous"></script>
 
-    <link rel="stylesheet" href="{{ asset('assets/css/adminUniv-afterPayment/Riwayat.css') }}">
-    <div class="kiri-putih p-5">
-        <a href="/RiwayatJangkaWaktu" style="text-decoration: none;">
-            <button class="button-with-icon jangkawaktu"><i class="fas fa-history"></i> Jangka Waktu</button>
-        </a>
-        <a href="/RiwayatPembelian" style="text-decoration: none;" >
-            <button class="button-with-icon riwayatpembelian {{ Request::is('RiwayatPembelian') ? 'active' : '' }}"><i class="fas fa-history"></i> Riwayat Pembelian</button>
-        </a>
+<link rel="stylesheet" href="{{ asset('assets/css/adminUniv-afterPayment/Riwayat.css') }}">
+<div class="kiri-putih p-5">
+    <a href="/RiwayatJangkaWaktu" style="text-decoration: none;">
+        <button class="button-with-icon jangkawaktu"><i class="fas fa-history"></i> Jangka Waktu</button>
+    </a>
+    <a href="/RiwayatPembelian" style="text-decoration: none;">
+        <button class="button-with-icon riwayatpembelian {{ Request::is('RiwayatPembelian') ? 'active' : '' }}"><i class="fas fa-history"></i> Riwayat Pembelian</button>
+    </a>
+</div>
+<div class="isie">
+    <h3 style="padding-left: 20%">Riwayat Pembelian</h3>
+    @foreach ($paket as $data)
+    <div class="kartu">
+        <p class="stt">Status</p>
+        <p class="aktif">
+            @if ($data->status == 'Aktif')
+            <img src="assets/images/check_circle.png" class="ceklis">
+            @else
+            <img src="assets/images/cancel.png" class="ceklis">
+            @endif
+            <b>{{ $data->status }}</b>
+        </p>
+        <p class="np">No Pesanan</p>
+        <p class="nnp"><b>{{ $data->no_pesanan }}</b></p>
+        <p class="H">Harga</p>
+        <p class="jt"><b>Rp. {{ $data->harga }}</b></p>
+        <p class="pkt2">Paket</p>
+        <p class="brz"><b>{{ $data->paket }}</b></p>
+        <p class="tgl">Tanggal</p>
+        <p class="ttgl"><b>{{ $data->tanggal }}</b></p>
+        <p class="mp">Metode Pembayaran</p>
+        <p class="mtp"><img src="{{ asset('assets/images/' . $data->metode_bayar . '.png') }}" class="lb"><b>{{ $data->metode_bayar }}</b></p>
+        <p class="acr"><img src="assets/images/arrow_circle_right.png">
     </div>
-    <div class="isie">
-        <h3 style="padding-left: 20%">Riwayat Pembelian</h3>
-        @foreach ($paket as $data)
-            <div class="kartu">
-                <p class="stt">Status</p>
-                <p class="aktif">
-                    @if ($data->status == 'Aktif')
-                        <img src="assets/images/check_circle.png" class="ceklis">
-                    @else
-                        <img src="assets/images/cancel.png" class="ceklis">
-                    @endif
-                    <b>{{ $data->status }}</b>
-                </p>
-                <p class="np">No Pesanan</p>
-                <p class="nnp"><b>{{ $data->no_pesanan }}</b></p>
-                <p class="H">Harga</p>
-                <p class="jt"><b>Rp. {{ $data->harga }}</b></p>
-                <p class="pkt2">Paket</p>
-                <p class="brz"><b>{{ $data->paket }}</b></p>
-                <p class="tgl">Tanggal</p>
-                <p class="ttgl"><b>{{ $data->tanggal }}</b></p>
-                <p class="mp">Metode Pembayaran</p>
-                <p class="mtp"><img src="{{ asset('assets/images/' . $data->metode_bayar . '.png') }}"
-                        class="lb"><b>{{ $data->metode_bayar }}</b></p>
-                <p class="acr"><img src="assets/images/arrow_circle_right.png">
-            </div>
-        @endforeach
-    </div>
+    @endforeach
+</div>
 @endsection
